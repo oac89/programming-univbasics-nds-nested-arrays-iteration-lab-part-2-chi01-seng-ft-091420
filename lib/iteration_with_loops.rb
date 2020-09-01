4f71ff1b40cda65621d38c924e2d32d007552b72
Oscar A Castro <oacastro2@gmail.com>
2020-09-01 21:43:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,5 @@
-
-def find_min_in_nested_arrays(src)
+array_of_daily_temperatures = [
   
-  array_of_daily_temperatures = [
   [19, 21, 24, 26, 30, 34, 37, 39, 40, 45, 48, 50, 55, 60, 63, 59, 49, 45, 40, 39, 34, 32, 25, 18],
   [17, 18, 22, 26, 31, 35, 38, 40, 43, 45, 49, 52, 55, 61, 60, 59, 55, 49, 45, 38, 32, 30, 24, 19],
   [13, 15, 20, 23, 33, 37, 39, 41, 45, 48, 50, 53, 57, 62, 61, 58, 52, 48, 44, 36, 35, 31, 23, 20],
@@ -9,30 +7,29 @@ def find_min_in_nested_arrays(src)
   [21, 25, 28, 30, 35, 41, 47, 49, 50, 55, 56, 57, 58, 64, 63, 58, 49, 44, 42, 36, 33, 33, 27, 20],
   [25, 29, 30, 36, 39, 45, 49, 50, 52, 57, 58, 59, 60, 66, 65, 59, 54, 49, 45, 40, 36, 30, 26, 24],
   [30, 36, 38, 39, 40, 45, 47, 49, 50, 59, 60, 60, 65, 63, 61, 56, 49, 43, 40, 39, 35, 32, 23, 22],
-]
+  ]
 
-  
-  
-  
-  
-  
-  lowest_array = [ ]
-  row_index = 0 
-  while row_index < array_of_daily_temperatures.count do 
-    element_index = 0 
-      lowest_integer_element = [] 
-      while element_index < array_of_daily_temperatures[row_index].count do 
-        
-        if array_of_daily_temperatures[row_index][element_index] = numbers.element_index.min 
-         
-        end 
+def find_min_in_nested_arrays (src)
+     
+      lowest_array = [ ]
+      row_index = 0 
+    
+    while row_index < array_of_daily_temperatures.count  
+        element_index = 0 
+     end 
+     
+     lowest_element =  array_of_daily_temperatures[row_index][0] 
+     
+      while element_index < array_of_daily_temperatures[row_index].count 
+      end 
+        if array_of_daily_temperatures[row_index][element_index] < lowest_element 
+          lowest_element = array_of_daily_temperatures[row_index][element_index] 
+          end
+      
+      
         element_index += 1
       end 
-      
-      lowest_array << lowest_integer_element 
-      
-end     
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-p lowest_array
-end 
+      lowest_array << lowest_element 
+      row_index +=1
+  end   
+  find_min_in_nested_arrays
