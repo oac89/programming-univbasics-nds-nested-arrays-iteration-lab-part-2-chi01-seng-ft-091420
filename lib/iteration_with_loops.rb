7305fe25adc28094c2ae6ca560a3c785c9f71c6c
Oscar A Castro <oacastro2@gmail.com>
2020-09-01 21:47:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,12 +6,12 @@ def find_min_in_nested_arrays (src)
     
     while row_index < array_of_daily_temperatures.count  
         element_index = 0 
-     end 
+      
      
      lowest_element =  array_of_daily_temperatures[row_index][0] 
      
       while element_index < array_of_daily_temperatures[row_index].count 
-      end 
+      
         if array_of_daily_temperatures[row_index][element_index] < lowest_element 
           lowest_element = array_of_daily_temperatures[row_index][element_index] 
           end
@@ -21,5 +21,7 @@ def find_min_in_nested_arrays (src)
       end 
       lowest_array << lowest_element 
       row_index +=1
+  end 
+  lowest_array 
 end   
   
